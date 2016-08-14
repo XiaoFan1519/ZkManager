3291a690d581f39d641a08a5e21c8bc33ef258c8
XiaoFan1519 <xiaofanms@outlook.com>
2016-08-14 13:37:53 +0800

    完成Config类的编写，并新增该类的测试用例。

---
+++ Zk小助手/Config.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ZkManager
+{
+    public class Config
+    {
+        private IDictionary<string, string> configs = 
+            new Dictionary<string, string>();
+
+        public bool Remove(string key)
+        {
+            return configs.Remove(key);
+        }
+
+        public string this[string key]
+        {
+            get
+            {
+                if (this.Contains(key))
+                {
+                    return configs[key];
+                }
+                return null;
+            }
+            set
+            {
+                if (Contains(key))
+                {
+                    configs[key] = value;
+                }
+                else
+                {
+                    configs.Add(key, value);
+                }
+            }
+        }
+
+        public bool Contains(string key)
+        {
+            return configs.ContainsKey(key);
+        }
+
+        /// <summary>
+        /// 读取配置文件，并保存到内存中
+        /// </summary>
+        /// <param name="path">文件路径</param>
+        public void load(string path)
+        {
+            StreamReader sr = new StreamReader(path);
+            string line = null;
+            while ((line = sr.ReadLine()) != null)
+            {
+                // 防止value里面也有=
+                int index = line.IndexOf('=');
+                if (index == -1)
+                {
+                    continue;
+                }
+
+                string key = line.Substring(0, index);
+                string value = line.Substring(index + 1);
+                this[key] = value;
+            }
+
+            sr.Close();
+        }
+
+        public void save(string path)
+        {
+            StreamWriter sw = new StreamWriter(path, false);
+
+            foreach (var entry in configs)
+            {
+                string line = string.Format("{0}={1}", entry.Key, entry.Value);
+                sw.WriteLine(line);
+            }
+
+            sw.Close();
+        }
+    }
+}
---
+++ Zk小助手Tests/ConfigTests.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ZkManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ZkManager.Tests
+{
+    [TestClass()]
+    public class ConfigTests
+    {
+        private Config configs = new Config();
+
+        [ClassInitialize()]
+        public static void init(TestContext testContext)
+        {
+            FileStream fs = File.Create("test.cfg");
+            StreamWriter sw = new StreamWriter(fs);
+            sw.WriteLine("xf=fx");
+            sw.WriteLine("111=AAA");
+            sw.WriteLine("222=BBB");
+            sw.Close();
+        }
+
+        [ClassCleanup()]
+        public static void clearUp()
+        {
+            File.Delete("test.cfg");
+        }
+
+        [TestInitialize()]
+        public void before()
+        {
+            configs["1"] = "AAA";
+            configs["2"] = "BBB";
+            configs["3"] = "CCC";
+            configs["3"] = "DDD";
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            configs["4"] = "DDD";
+            Console.WriteLine(this.GetHashCode());
+        }
+
+        [TestMethod()]
+        public void RemoveTest()
+        {
+            bool ret = configs.Remove("1");
+            if (!ret)
+                Assert.Fail();
+
+            Console.WriteLine(this.GetHashCode());
+        }
+
+        [TestMethod()]
+        public void ContainsTest()
+        {
+            if (!configs.Contains("1"))
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod()]
+        public void loadTest()
+        {
+            configs.load("test.cfg");
+
+            string value = configs["111"];
+
+            if (value != "AAA")
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod()]
+        public void saveTest()
+        {
+            configs["saveTest"] = "Test";
+            configs.save("test.cfg");
+
+            configs.load("test.cfg");
+            string value = configs["saveTest"];
+            if (value != "Test")
+            {
+                Assert.Fail();
+            }
+
+            File.Delete("test.cfg");
+            configs.save("test.cfg");
+        }
+
+        [TestMethod()]
+        public void getTest()
+        {
+            string value = configs["3"];
+            if (value != "DDD")
+            {
+                Assert.Fail();
+            }
+
+            value = configs["111"];
+            if (value != null)
+            {
+                Assert.Fail();
+            }
+        }
+    }
+}
