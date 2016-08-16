4879f333061d5f3d1cbc515bb4e3b5412c877ff5
XiaoFan1519 <xiaofanms@outlook.com>
2016-08-16 22:19:02 +0800

    新增对zk的基础功能。

--- Zk小助手/ZkClient.cs
+++ Zk小助手/ZkClient.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 using System.Threading;
 using ZooKeeperNet;
 
@@ -32,6 +34,52 @@ namespace ZkManager
             {
                 isConnected = true;
             }
+        }
+
+        /// <summary>
+        /// 查找指定路径下的子节点
+        /// </summary>
+        /// <param name="path">指定路径</param>
+        /// <returns>路径下的子节点</returns>
+        public List<string> getChildren(string path)
+        {
+            IEnumerable<string> collection = zk.GetChildren(path, false);
+            List<string> list = new List<string>(collection);
+            list.Sort();
+            return list;
+        }
+
+        /// <summary>
+        /// 将strData已指定编码格式保存到zk上。默认strData的格式为UTF-8
+        /// </summary>
+        /// <param name="path">路径地址</param>
+        /// <param name="strData">要保存的文本</param>
+        /// <param name="dstEncode">指定编码格式</param>
+        public void setData(string path, string strData, Encoding dstEncode)
+        {
+            byte[] data = Encoding.Convert(Encoding.UTF8, dstEncode, strData.GetBytes());
+            zk.SetData(path, data, -1);
+        }
+
+        /// <summary>
+        /// 获取指定节点上的内容，根据指定的编码格式。
+        /// </summary>
+        /// <param name="path">节点路径</param>
+        /// <param name="dstEncode">指定的编码格式</param>
+        /// <returns></returns>
+        public string getData(string path, Encoding dstEncode)
+        {
+            byte[] data = zk.GetData(path, false, null);
+            return dstEncode.GetString(data);
+        }
+
+        /// <summary>
+        /// 删除指定节点，当有子节点时无法删除。
+        /// </summary>
+        /// <param name="path">节点路径</param>
+        public void delete(string path)
+        {
+            zk.Delete(path, -1);
         }
     }
 
