03da0bb4d11c94b762f2a358ff12562e0104e8ee
xiaofan <xiaofanms@outlook.com>
2016-10-03 14:01:27 +0800

    因为zookeeperNet内部有异常抛出，正常流程捕获不到，所以加上全局异常捕获。

--- Zk小助手/Program.cs
+++ Zk小助手/Program.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Windows.Forms;
 using ZkManager.constant;
+using System.Threading;
 
 namespace ZkManager
 {
@@ -24,6 +25,11 @@ namespace ZkManager
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
+            // UI线程异常
+            Application.ThreadException += Application_ThreadException;
+            // 多线程异常
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
             Config config = new Config();
             config.Load(Constant.ZK_CFG);
             Login loginFrm = new Login(config);
@@ -39,6 +45,30 @@ namespace ZkManager
             {
                 config.Save(Constant.ZK_CFG);
             }
+        }
+
+        /// <summary>
+        /// 多线程异常
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Exception ex = e.ExceptionObject as Exception;
+            log.Error("UnhandledException", ex);
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// UI线程异常
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            Exception ex = e.Exception;
+            log.Error("ThreadException", ex);
+            throw new NotImplementedException();
         }
     }
 }
